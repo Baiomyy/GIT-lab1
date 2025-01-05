e3af84f9960e4c5f06245ad024575b0d3df10409
Ahmed Baioumy <ahmedghoname00@gmail.com>
2025-01-05 12:28:44 +0200

    insert printHelloGit method in HelloITI class

--- HelloITI.cpp
+++ HelloITI.cpp
@@ -1,1 +1,20 @@
 
+
+#include <iostream>
+
+using namespace std;
+
+
+class HelloITI
+{
+public:
+    void printHelloITI()
+    {
+        cout << "Hello, ITI!" << endl;
+    }
+
+    void printHelloGit()
+    {
+        cout << "Hello, GIT!" << endl;
+    }
+};
